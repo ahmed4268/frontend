701e939cee15c2e0b949367f4c38e8bbd5f5953a
ahmed4268 <ahmed.elkhoualed@fsb.ucar.tn>
2024-05-06 06:27:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,170 @@ import './App.css';
 import Newoperation from "./Routes/Newoperation/Newoperation";
 import Dachboard from "./Routes/dachbord/dashbord";
 import Archive from "./Routes/Archive/Archive";
+import Replay from "./Routes/Archive/replay";
 import Tracking from "./Routes/tracking/tracking";
 import Editopperation from "./component/editoperation/editopperation";
+import Tech from "./Plannificateur/tech/tech";
+import Conge from "./Plannificateur/conge/conge";
+import Veh from "./Plannificateur/veh/veh";
+import Track from "./Plannificateur/veh/track";
+import Site from "./Plannificateur/site/track";
+import EditTech from "./Plannificateur/tech/edittech";
+import Editveh from "./Plannificateur/veh/editveh";
+import Editconge from "./Plannificateur/conge/editconge";
+import NewTech from "./Plannificateur/tech/newtech";
+import Time from "./Plannificateur/tech/time";
+import Newconge from "./Plannificateur/conge/newconge";
+import Newveh from "./Plannificateur/veh/newveh";
+import NotAuthorized from './login_logout/NotAuthorized';
+import Notauthorized from './login_logout/not-authentificated';
+// import RoleBasedRoute from './store/RoleBasedRoute';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store/store';
+import Login from "./login_logout/login";
+import ResetPassword from "./login_logout/paswordreset";
+import Profil from "./login_logout/Pofil";
+import Users from "./Users/User";
+import Cheftable from "./Users/Operations_chef";
+import {useEffect} from "react";
+import Cookies from "js-cookie";
+
+// import Socket from "./Routes/tracking/socket";
 const App=() => {
+    let user=null;
+    useEffect(() => {
+        if (localStorage.getItem('user') && !Cookies.get('token')) {
+            localStorage.removeItem('user');
+        }
+    }, []);
+    if  (Cookies.get('token') && !localStorage.getItem('user')) {
+        Cookies.remove('token');
+    }
+    if(localStorage.getItem('user')){
+     user = JSON.parse(localStorage.getItem('user'));}
   return (
 
-
+      <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
           <Routes>
+              <Route path="/resetPassword/:token" element={<ResetPassword/>}/>
+              <Route path="*" element={<Login />} />
+              {user ? (
+                  <>
+                      {user.role === 'admin' && (
+                          <>
 
-              <Route index element={<Dachboard />} />
+                              <Route path="/resetPassword/:token" component={ResetPassword} />
+                              <Route path="/" element={<Dachboard />} />
               <Route path="/archive" element={<Archive />} />
+              <Route path="/not-authorized" element={<Notauthorized/>} />
+              <Route path="/not-authentificated" element={<Notauthorized/>} />
+              <Route path="/Profil" element={<Profil />} />
+              <Route path="/Users" element={<Users />} />
+              <Route path="/chef_operations" element={< Cheftable/>} />
+              <Route path="/replay" element={<Replay />} />
+              <Route path="/track" element={<Track />} />
+              <Route path="/technicians" element={<Tech />} />
+              <Route path="/sites" element={<Site />} />
+              <Route path="/congés" element={<Conge />} />
+              <Route path="/vehicules" element={<Veh/>} />
               <Route path="/new-operation" element={<Newoperation/>} />
               <Route path="/editoperation" element={<Editopperation/>} />
+              <Route path="/edittech" element={<EditTech/>} />
+              <Route path="/presence" element={<Time/>} />
+              <Route path="/editveh" element={<Editveh/>} />
+              <Route path="/editconge" element={<Editconge/>} />
+              <Route path="/new-technician" element={<NewTech/>} />
+              <Route path="/newconge" element={<Newconge/>} />
+              <Route path="/new-vehicule" element={<Newveh/>} />
+
               <Route path="/tracking" element={<Tracking/>} />
+                              <Route path="*" element={<NotAuthorized />}/>
 
+                          </>
+                      )}
+                      {user.role === 'ChefProjet' && (
+                          <>
+                              <Route path="/resetPassword/:token" component={ResetPassword} />
+                              <Route path="/Profil" element={<Profil />} />
+
+                              <Route index element={<Dachboard />} />
+                              <Route path="*" element={<NotAuthorized />}/>
+                              <Route path="/archive" element={<Archive />} />
+                              <Route path="/not-authorized" element={<Notauthorized/>} />
+                              <Route path="/not-authentificated" element={<Notauthorized/>} />
+                              <Route path="/login" element={<Login />} />
+                              <Route path="/replay" element={<Replay />} />
+                              <Route path="/new-operation" element={<Newoperation/>} />
+                              <Route path="/editoperation" element={<Editopperation/>} />
+                              <Route path="/tracking" element={<Tracking/>} />
+
+
+
+                              <Route path="/not-authorized" element={<Notauthorized/>} />
+                              <Route path="/replay" element={<Notauthorized/>} />
+                              <Route path="/track" element={<Notauthorized/>} />
+                              <Route path="/technicians" element={<Notauthorized />} />
+                              <Route path="/sites" element={<Notauthorized />} />
+                              <Route path="/congés" element={<Notauthorized />} />
+                              <Route path="/vehicules" element={<Notauthorized/>} />
+
+
+                              <Route path="/edittech" element={<Notauthorized/>} />
+                              <Route path="/presence" element={<Notauthorized/>} />
+                              <Route path="/editveh" element={<Notauthorized/>} />
+                              <Route path="/editconge" element={<Notauthorized/>} />
+                              <Route path="/new-technician" element={<Notauthorized/>} />
+                              <Route path="/newconge" element={<Notauthorized/>} />
+                              <Route path="/new-vehicule" element={<Notauthorized/>} />
+
+
+                          </>
+                      )}
+                      {user.role === 'Planifcateur' && (
+                          <>
+                              <Route path="/resetPassword/:token" component={ResetPassword} />
+
+                              <Route path="/Profil" element={<Profil />} />
+
+                              <Route index element={<Tech />} />
+                              <Route path="*" element={<NotAuthorized />}/>
+                              <Route path="/not-authorized" element={<Notauthorized/>} />
+                              <Route path="/not-authentificated" element={<Notauthorized/>} />
+                              <Route path="/login" element={<Login />} />
+                              <Route path="/replay" element={<Replay />} />
+                              <Route path="/track" element={<Track />} />
+                              <Route path="/technicians" element={<Tech />} />
+                              <Route path="/sites" element={<Site />} />
+                              <Route path="/congés" element={<Conge />} />
+                              <Route path="/vehicules" element={<Veh/>} />
+                              <Route path="/edittech" element={<EditTech/>} />
+                              <Route path="/presence" element={<Time/>} />
+                              <Route path="/editveh" element={<Editveh/>} />
+                              <Route path="/editconge" element={<Editconge/>} />
+                              <Route path="/new-technician" element={<NewTech/>} />
+                              <Route path="/newconge" element={<Newconge/>} />
+                              <Route path="/new-vehicule" element={<Newveh/>} />
+
+                              <Route path="/archive" element={<Notauthorized />} />
+                              <Route path="/not-authentificated" element={<Notauthorized/>} />
+
+                              <Route path="/replay" element={<Notauthorized />} />
+                              <Route path="/new-operation" element={<Notauthorized/>} />
+                              <Route path="/editoperation" element={<Notauthorized/>} />
+                              <Route path="/tracking" element={<Notauthorized/>} />
+
+                          </>
+                      )}
+
+                {/*<Route path="/socket" element={<Socket/>} />*/}
+                  </>
+              ) : <Route path="*" element={<Login />} />
+              }
           </Routes>
-
+          </PersistGate>
+      </Provider>
 
 
 
--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -2,20 +2,244 @@
 import { ReactComponent as Logo } from './logo.svg';
 import  './navbar.scss';
 import {
-    PlusOutlined ,
-    BellOutlined ,
+    PlusOutlined,
+    BellOutlined, UserOutlined, LogoutOutlined,
 } from '@ant-design/icons';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { addNotification } from '../../actions/notificationActions'; // import your action creator
 
-import {Space, Tooltip, Layout, theme, Button, Badge} from 'antd';
+import {Space, Tooltip, Layout, theme, Button, Badge, Menu, Dropdown, message} from 'antd';
+import React, { useEffect } from 'react';
+
+import UpdateEvents from './updateEvents';
+import { removeToken,logout } from '../../actions/cookieActions';
+import Cookies from "js-cookie";
+import {useNavigate} from "react-router-dom";
 
 const { Header } = Layout;
 
 
 const Navbar = () => {
+
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
+    const dispatch = useDispatch();
+    const user=JSON.parse(localStorage.getItem('user'));
+    const role=user.role;
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(removeToken());
+        localStorage.removeItem('user');
+
+        Cookies.remove('token');
+        Cookies.remove('jwwt');
+        navigate('/login')
+    };
+    const notifications = useSelector(state => state.operationNotifications.notifications);
+    console.log('notifications',notifications)
+
+
+        const testNotifications = () => {
+            // Operation Started
+            dispatch(addNotification({
+                message: 'Operation Started',
+                description: `Operation: Test Operation has started.`,
+                type: 'info'
+            }));
+
+            // Operation Ended
+            dispatch(addNotification({
+                message: 'Operation Ended',
+                description: `Operation: Test Operation has ended.`,
+                type: 'info'
+            }));
+
+            // Technician did not enter
+            dispatch(addNotification({
+                message: 'Technician did not enter',
+                description: `Technician: Test Technician did not enter the site.`,
+                type: 'warning'
+            }));
+
+            // Technician left early
+            dispatch(addNotification({
+                message: 'Technician left early',
+                description: `Technician: Test Technician left the site early.`,
+                type: 'warning'
+            }));
+        };
+
+    const menu = (
+        <Menu style={{ maxHeight: '300px', overflow: 'auto' }}>
+            {notifications && notifications.map((notification, index) => (
+                <Menu.Item key={index}>
+                    <div>
+                        <strong>{notification.message}</strong>
+                        <p>{notification.description}</p>
+                    </div>
+                </Menu.Item>
+            ))}
+            <Menu.Divider />
+            <Menu.Item onClick={() => dispatch({ type: 'CLEAR_NOTIFICATIONS' })}>
+                Clear Notifications
+            </Menu.Item>
+        </Menu>
+    );
+    const NotificationButton = () => {
+        if (notifications.length === 0) {
+            return (
+                <Button shape="around" icon={<BellOutlined />} style={{
+                    background:colorBgContainer,
+                }}  onClick={() => message.info('Notification box is empty')} />
+            );
+        } else {
+            return (
+                <Dropdown overlay={menu} trigger={['click']}>
+                    <Badge size="small" color="red">
+                        <Button shape="around"  style={{
+                            background:colorBgContainer,
+                        }} icon={<BellOutlined />} />
+                    </Badge>
+                </Dropdown>
+            );
+        }
+    };
+    // const [operations, setOperations] = useState([]);
+    // const credentials = btoa('mohamedouesalti080@gmail.com:RZedi!Z9MpqnF@K');
+    // const [storedEvents, setStoredEvents] = useState([]);
+    //
+    // const [events, setEvents] = useState({});
+    // useEffect(() => {
+    //
+    //     const fetchOperations = async () => {
+    //         const source = axios.CancelToken.source();
+    //         try {
+    //             const { data } = await axios.get('/operation/map', {
+    //                 cancelToken: source.token,
+    //             });
+    //             setOperations(data);
+    //         } catch (error) {
+    //             if (axios.isCancel(error)) {
+    //                 console.log('Request canceled', error.message);
+    //             } else {
+    //                 console.log('Error', error.message);
+    //             }
+    //         }
+    //
+    //
+    //     };
+    //
+    //     fetchOperations();
+    // }, []);
+    // console.log('operations',operations)
+    //
+    //     useEffect(() => {
+    //         const fetchEvents = async () => {
+    //             // Create a new CancelToken source for this request
+    //             console.log('we fetch events');
+    //             const source = axios.CancelToken.source();
+    //
+    //             try {
+    //                 // For each operation, fetch events
+    //                 for (const operation of operations) {
+    //                     console.log('we on trracar',operation);
+    //                     const { group, startTime, endTime } = operation;
+    //                     const eventsUrl = `https://demo4.traccar.org/api/reports/events?groupId=${group}&from=${startTime}&to=${endTime}&type=geofenceEnter&type=geofenceExit`;
+    //                     const { data } = await axios.get(eventsUrl, {
+    //                         headers: {
+    //                             'Authorization': `Basic ${credentials}`
+    //                         },
+    //                         cancelToken: source.token
+    //                     });
+    //                    console.log("event traccar",data);
+    //
+    //
+    //
+    //                     if(events[operation._id] && events[operation._id].length > 0) {
+    //                         console.log("we enter")
+    //                         const newEvents = data.filter(newEvent => !events[operation._id].some(event => event.id === newEvent.id));
+    //                         console.log("newevents", newEvents)
+    //                         if (newEvents.length > 0) {
+    //                             for (const newEvent of newEvents) {
+    //                                 notification.open({
+    //                                     message: 'New Event',
+    //                                     description: `A new event has occurred in the active operation. Device ID: ${newEvent.deviceId}, Type: ${newEvent.type}, Event Time: ${newEvent.eventTime}`,
+    //                                 });
+    //                             }
+    //                         }
+    //                     }
+    //                     // Update the events state
+    //                     setEvents(prevEvents => ({
+    //                         ...prevEvents,
+    //                         [operation._id]: [...(prevEvents[operation.id] || []), ...data]
+    //                     }));                    }
+    //             } catch (error) {
+    //                 if (axios.isCancel(error)) {
+    //                     console.log('Request canceled', error.message);
+    //                 } else {
+    //                     // handle error
+    //                     console.error(error);
+    //                 }
+    //             }
+    //             setTimeout(fetchEvents, 5000);
+    //         };
+    //
+    //         if (operations.length > 0) {
+    //             fetchEvents(); // Fetch events when operations are available
+    //             // Clean up interval on component unmount
+    //             }
+    //
+    //     }, [operations]);
+    if(role==="Planifcateur"){
+        return(
+
+            <Header
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height:'15vh',
+                    justifyContent: 'space-between',
+                    borderRadius: borderRadiusLG,
+                    margin:"8px"
+                }}
+            >
+                <a className='logo-container' href='/'>
+                    <Logo  className='logo'/>
+                </a>
+                <Space size={"middle"}>
+
+
+                    <Tooltip title="Profil">
+                        <Button shape="raound" href="/profil"  icon={<UserOutlined />}
+                                style={{
+                                    background:colorBgContainer,
+                                }} />
+
+
+                    </Tooltip>
+                    <Tooltip title="LogOut">
+                        <Button shape="raound" onClick={handleLogout} icon={<LogoutOutlined />}
+                                style={{
+                                    background:colorBgContainer,
+                                }} />
+
+
+                    </Tooltip>
+
+
+                </Space>
+            </Header>
+
+        )
+    }else{
     return(
+        <>
+            <UpdateEvents />
         <Header
             style={{
                 display: 'flex',
@@ -38,18 +262,12 @@ const Navbar = () => {
                         }}>New Operation</Button>
 
                 <Tooltip title="Notification">
-                    <Badge size="small" color="red" count={8}>
-                    <Button shape="raound" icon={<BellOutlined />}
-                            style={{
-                                background:colorBgContainer,
-                            }} />
-                    </Badge>
-
+                    <NotificationButton />
                 </Tooltip>
-
-
+                {/*<button onClick={testNotifications}>Test Notifications</button>*/}
             </Space>
         </Header>
+        </>
     );
-};
+};}
 export default Navbar ;
